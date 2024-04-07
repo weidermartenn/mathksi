dbbe1453f09ef0aa8b9365bf342c04e5a6f9e9df
weidermartenn <weidermartenn@gmail.com>
2024-04-07 22:44:40 +0300

    dsf

--- src/components/FooterBar/FooterBar.jsx
+++ src/components/FooterBar/FooterBar.jsx
@@ -3,8 +3,8 @@ import './FooterBar.css'
 export default function Footer(){
     return(
         <footer>
-            <span>АВТОРЫ: СТУДЕНТЫ ГРУППЫ С122</span>
-            <span>КРИВЁНЫШЕВ ОЛЕГ, КИРИЛЛ ГОРЯЧКИН</span>
+            <span>© 2024</span>
+            <span>Все права защищены</span>
         </footer>
     )
 }
