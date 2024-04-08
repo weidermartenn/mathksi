d83e4d6e45395fbee4c502c427ffd51b98a6709a
weidermartenn <weidermartenn@gmail.com>
2024-04-08 19:27:00 +0300

    asdasdasd

--- src/components/MethodsContainer/MethodsContainer.jsx
+++ src/components/MethodsContainer/MethodsContainer.jsx
@@ -3,35 +3,15 @@ import './MethodsContainer.css';
 export default function MethodsContainer(){
     return(
         <div className="container">
-            <div className="container-inline">
-                <span>МЕТОДЫ РЕШЕНИЯ УРАВНЕНИЙ</span>
-                <div className='methods-cards'>
-                    <div className="card">
-                        <span>МЕТОД ПОЛОВИННОГО ДЕЛЕНИЯ</span>
-                    </div>
-                    <div className="card">
-                        <span>МЕТОД ХОРД</span>
-                    </div>
-                    <div className="card">
-                        <span>МЕТОД КАСАТЕЛЬНЫХ</span>
-                    </div>
-                    <div className="card">
-                        <span>МЕТОД ИТЕРАЦИЙ</span>
-                    </div>
+            <span>МЕТОДЫ РЕШЕНИЯ СИСТЕМЫ УРАВНЕНИЙ</span>
+            <div className="container-met">
+                <div className='container-column'> 
+                    <button className="card">МЕТОД ИТЕРАЦИЙ</button>
+                    <button className="card">МЕТОД ГАУССА</button>
+                    <button className="card">МЕТОД ЗЕЙДЕЛЯ</button> 
                 </div>
-            </div>
-            <div className="container-inline">
-                <span>МЕТОДЫ РЕШЕНИЯ СИСТЕМЫ УРАВНЕНИЙ</span>
-                <div className='methods-cards'>
-                    <div className="card">
-                        <span>МЕТОД ГАУССА</span>
-                    </div>
-                    <div className="card">
-                        <span>МЕТОД ИТЕРАЦИЙ</span>
-                    </div>
-                    <div className="card">
-                        <span>МЕТОД ЗЕЙДЕЛЯ</span>
-                    </div>
+                <div className='container-method'>
+
                 </div>
             </div>
         </div>
